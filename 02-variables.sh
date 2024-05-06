57af0bf19602db8fbc6be0d70e00e9a84d453f7d
hasamahas <hasan.shaik3@gmail.com>
2024-05-06 14:37:05 +0530

    variable-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,5 @@
-* this I dont want to push
+#!/bin/bash
+echo "Ramesh: Hello suresh how are you"
+echo "Suresh: Im fine. How are you"
+echo "Ramesh: Im fine too. How is your work"
+echo "Suresh: Not bad. Im thinking to upgrade to devops"
