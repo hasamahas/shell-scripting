db7da349b4400217c6225b4ebaac9e7ae7078b27
hasamahas <hasan.shaik3@gmail.com>
2024-05-06 22:25:12 +0530

    variable-script2

--- 11-installation.sh
+++ 11-installation.sh
@@ -11,6 +11,24 @@ fi
     
 dnf install mysql -y
 
+if[ $? -ne 0 ]
+then
+    echo "Installation of mysql is ...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql is ...SUCCESS"
+fi
+
+dnf install git -y
+
+if[ $? -ne 0]
+then
+    echo "Installation of git is ...FAILURE"
+    exit 1
+else
+    echo "Installation of git is ...SUCCESS"
+fi
+
 echo "Is script proceeding?"
 
 # $? to check the exit status of previous command
