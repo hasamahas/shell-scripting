3bc25f10587f87ac98627c0f1c2764b3f8090ec6
hasamahas <hasan.shaik3@gmail.com>
2024-05-07 12:28:44 +0530

    variable-script2

--- 17-install-packages.sh
+++ 17-install-packages.sh
@@ -4,6 +4,19 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
@@ -18,9 +31,11 @@ do
     dnf list installed $i &>>LOGFILE
     if [ $? -eq 0 ]
     then
-        echo "$i already installed ....SKIPPING"
+        echo "$i already installed ....$Y SKIPPING $N"
     else
-        echo "$i not installed... NEED TO INSTALL"
+        dnf install mysql -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+
     fi
 
 done
