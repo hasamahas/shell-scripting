f57dc63e068a3e2f55b6571630ec36f6089114ad
hasamahas <hasan.shaik3@gmail.com>
2024-05-07 11:45:09 +0530

    variable-script2

---
+++ 17-install-packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+for i in $@
+do  
+    echo "package to isntall is: $i"
+done
