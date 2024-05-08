997a58fdfc9a193332c5809a714127690f8c2a38
hasamahas <hasan.shaik3@gmail.com>
2024-05-08 14:22:10 +0530

    shell calling

---
+++ 18-current.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops in current script"
+
+echo "Before callig other script Course: $COURSE"
+echo "Process instance id of current script: $$"
+
+#./19-other-script.sh
+
+echo "After calling other script Course: $COURSE"
