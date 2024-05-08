1015d475c61c913d3960bb933e703e3a83a40a12
hasamahas <hasan.shaik3@gmail.com>
2024-05-08 14:07:35 +0530

    expense project

--- 18-current-script.sh
+++ 18-current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops in current script"
 echo "Before callig other script Course: $COURSE"
 echo "Process instance id of current script: $$"
 
-./19-other-script.sh
+#./19-other-script.sh
 
 echo "After calling other script Course: $COURSE"
 
