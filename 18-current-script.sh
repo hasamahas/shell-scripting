0bc38450bc1ea5ec01288b753ab9b7e605633388
hasamahas <hasan.shaik3@gmail.com>
2024-05-08 13:55:23 +0530

    expense project

---
+++ 18-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/basah
+
+COURSE="Devops in current script"
+
+echo "Variable value from current script"
+
+#./19-other-script.sh
+
+echo "After calling other script"
+
---
+++ 19-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="DevOps from other script"
+
+echo "Variable value from other script"
+echo "Process instance id of other script: $$"
