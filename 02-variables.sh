428a14133c5f93bbb3b4343ef1e49451bf0a4884
hasamahas <hasan.shaik3@gmail.com>
2025-08-20 09:22:15 +0530

    shell-scripting

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hasan: Hello suresh how are you"
-echo "Suresh: Im fine. How are you"
+echo "Ravi: Im fine. How are you"
 echo "Hasan: Im fine too. How is your work"
-echo "Suresh: Not bad. Im thinking to upgrade to devops"
-echo "WoW! that is good decision"
+echo "Ravi: Not bad. Im thinking to upgrade to devops"
+echo "Hasan: WoW! that is good decision"
