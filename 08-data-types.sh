c85d13ba8abe955c8ed1e09328c1094541511ac6
hasamahas <hasanshaik3@gmail.com>
2026-04-10 13:26:07 +0530

    08 file update

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -4,6 +4,8 @@ NO1=$1
 NO2=$2
 
 SUM=$(($NO1+$NO2))
+MUL=$(($NO1*$NO2))
 
 echo "Total of $NO1 and $NO2 is: $SUM"
+echo "Product of $NO1 and $NO2 is: $MUL"
 # if you give text instead of number shell will consider as 0
