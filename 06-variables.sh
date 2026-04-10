63863257188e67336c492700b018319a86508f8c
hasamahas <hasanshaik3@gmail.com>
2026-04-10 13:21:00 +0530

    06 file update

--- 06-variables.sh
+++ 06-variables.sh
@@ -6,6 +6,6 @@ read USERNAME #here USERNAME is variable and -s for hide the credentials
  
 #echo "Please enter password::"
 
-read  -p "Enter the password:" -s PASSWORD
+read  "Enter the password:" -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
