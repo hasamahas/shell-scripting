2acc70a053beef84cc5a7c4618c59b78fd520da0
hasamahas <hasanshaik3@gmail.com>
2026-09-10 05:22:35 +0300

    changes in shell scripting

--- 20-delete-old-log.sh
+++ 20-delete-old-log.sh
@@ -2,6 +2,30 @@
 
 SOURCE_DIRECTORY=/tmp/app-logs
 
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+
+# if [ -d $SOURCE_DIRECTORY ]
+# then
+#     echo -e "$G Source directory exists $N"
+# else
+#     echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+#     exit 1
+# fi
+
+# FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+# while IFS= read -r line
+# do
+#     echo "Deleting file: $line"
+#     rm -rf $line
+# done <<< $FILES
+
+
+SOURCE_DIRECTORY="/tmp/app-logs
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -9,16 +33,15 @@ N="\e[0m"
 
 if [ -d $SOURCE_DIRECTORY ]
 then
-    echo -e "$G Source directory exists $N"
+ echo -e "$G Source directory exists $N"
 else
-    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
-    exit 1
-fi
+ echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
+if
 
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-while IFS= read -r line
+while IFS= read -f line
 do
-    echo "Deleting file: $line"
-    rm -rf $line
-done <<< $FILES
+ echo "deleting file:$line"
+ rm -rf $line
+done <<<$FILES
