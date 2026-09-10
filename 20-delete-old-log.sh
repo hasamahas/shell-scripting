a9c628d8e3228b99083d241c4cf3f942a4779782
hasamahas <hasanshaik3@gmail.com>
2026-09-10 05:34:03 +0300

    changes in shell scripting

--- 20-delete-old-log.sh
+++ 20-delete-old-log.sh
@@ -31,11 +31,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ -d $SOURCE_DIRECTORY ]
+if [ -d "$SOURCE_DIRECTORY" ]
 then
- echo -e "$G Source directory exists $N"
+    echo -e "$G Source directory exists $N"
 else
- echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
+    echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
 if
 
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
